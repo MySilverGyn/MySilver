8594a8a107ef2c645b2cd63cb1c7509badda409d
MySilverGyn <oguiisilva@gmail.com>
2025-07-27 14:25:58 -0300

    Update filtro-categoria.js

--- script/filtro-categoria.js
+++ script/filtro-categoria.js
@@ -2,7 +2,7 @@
 const scriptTag = document.currentScript;
 const categoriaDesejada = scriptTag.dataset.categoria;
 
-fetch('../produtos.json')
+fetch('Produtos/produtos.json')
   .then(response => response.json())
   .then(produtos => {
     const container = document.getElementById('produtos-container');
